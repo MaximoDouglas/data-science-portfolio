3f4a1cebe1f15293bc73cb160745a0c45919a184
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-05-12 21:31:23 -0300

    Add args selector

--- mask-detection/code/detect_mask_image.py
+++ mask-detection/code/detect_mask_image.py
@@ -6,3 +6,18 @@ import numpy as np
 import argparse
 import cv2
 import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+ap.add_argument("-f", "--face", type=str,
+	default="face_detector",
+	help="path to face detector model directory")
+ap.add_argument("-m", "--model", type=str,
+	default="mask_detector.model",
+	help="path to trained face mask detector model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+print(args)
