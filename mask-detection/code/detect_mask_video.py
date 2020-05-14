a3e33240c6ce1667c91cb7f53ace01b274c8138f
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-05-13 22:43:03 -0300

    Add camera frames loop

--- mask-detection/code/detect_mask_video.py
+++ mask-detection/code/detect_mask_video.py
@@ -77,3 +77,42 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 	# return a 2-tuple of the face locations and their corresponding
 	# locations
 	return (locs, preds)
+
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 400 pixels
+	frame = vs.read()
+	frame = imutils.resize(frame, width=400)
+	# detect faces in the frame and determine if they are wearing a
+	# face mask or not
+	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+
+	# loop over the detected face locations and their corresponding
+	# locations
+	for (box, pred) in zip(locs, preds):
+		# unpack the bounding box and predictions
+		(startX, startY, endX, endY) = box
+		(mask, withoutMask) = pred
+		# determine the class label and color we'll use to draw
+		# the bounding box and text
+		label = "With Mask" if mask > withoutMask else "No Mask"
+		color = (0, 190, 0) if label == "With Mask" else (0, 0, 255)
+		# include the probability in the label
+		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+		# display the label and bounding box rectangle on the output
+		# frame
+		cv2.putText(frame, label, (startX, startY - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+    
+	# show the output frame
+	cv2.imshow("Doug M. - Face Mask Detector", frame)
+	key = cv2.waitKey(1) & 0xFF
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
