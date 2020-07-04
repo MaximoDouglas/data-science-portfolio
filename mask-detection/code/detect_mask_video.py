efe4619c762728a239d53ac7089ea81d8dd1c2b1
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-07-03 23:28:27 -0300

    Refactor and clean code for face mask detection on videos

--- mask-detection/code/detect_mask_video.py
+++ mask-detection/code/detect_mask_video.py
@@ -10,97 +10,71 @@ import time
 import cv2
 import os
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--face", type=str,
-	default="face_detector",
-	help="path to face detector model directory")
-ap.add_argument("-m", "--model", type=str,
-	default="mask_detector.model",
-	help="path to trained face mask detector model")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="minimum probability to filter weak detections")
-args = vars(ap.parse_args())
+argument_parser = argparse.ArgumentParser()
+argument_parser.add_argument("-f", "--face", type=str, default="face_detector", help="path to face detector model directory")
+argument_parser.add_argument("-m", "--model", type=str, default="mask_detector.model", help="path to trained face mask detector model")
+arguments = vars(argument_parser.parse_args())
 
-# load our serialized face detector model from disk
-print("[INFO] loading face detector model...")
-prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
-weightsPath = os.path.sep.join([args["face"],
-	"res10_300x300_ssd_iter_140000_fp16.caffemodel"])
-faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
-# load the face mask detector model from disk
-print("[INFO] loading face mask detector model...")
-maskNet = load_model(args["model"])
-# initialize the video stream and allow the camera sensor to warm up
-print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
+CONFIDENCE_THRESHOLD = 0.5
+
+# Load face detector
+prototxt_path = os.path.sep.join([arguments["face"], "deploy.prototxt"])
+weights_path  = os.path.sep.join([arguments["face"], "res10_300x300_ssd_iter_140000_fp16.caffemodel"])
+face_net 	  = cv2.dnn.readNet(prototxt_path, weights_path)
+
+# Load face mask detector
+mask_net = load_model(arguments["model"])
+
+# Video stream start
+video_stream = VideoStream(src=0).start()
 time.sleep(2.0)
 
-def detect_and_predict_mask(frame, faceNet, maskNet):
-	# grab the dimensions of the frame and then construct a blob
-	# from it
+def detect_faces_and_predict_mask(frame, face_net, mask_net):
 	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
-		(104.0, 177.0, 123.0))
-	# pass the blob through the network and obtain the face detections
-	faceNet.setInput(blob)
-	detections = faceNet.forward()
-	# initialize our list of faces, their corresponding locations,
-	# and the list of predictions from our face mask network
-	faces = []
-	locs = []
-	preds = []
+	blob   = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104.0, 177.0, 123.0))
+	
+	face_net.setInput(blob)
+	detections = face_net.forward()
+	
+	face_roi_list 	   = []
+	face_location_list = []
+	prediction_list    = []
 
-    # loop over the detections
 	for i in range(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the detection
 		confidence = detections[0, 0, i, 2]
-		# filter out weak detections by ensuring the confidence is
-		# greater than the minimum confidence
-		if confidence > args["confidence"]:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the object
+		
+		if confidence > CONFIDENCE_THRESHOLD:
 			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			
 			(startX, startY, endX, endY) = box.astype("int")
-			# ensure the bounding boxes fall within the dimensions of
-			# the frame
+			
 			(startX, startY) = (max(0, startX), max(0, startY))
-			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+			(endX, endY) 	 = (min(w - 1, endX), min(h - 1, endY))
 
-            # extract the face ROI, convert it from BGR to RGB channel
-			# ordering, resize it to 224x224, and preprocess it
-			face = frame[startY:endY, startX:endX]
-			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-			face = cv2.resize(face, (224, 224))
-			face = img_to_array(face)
-			face = preprocess_input(face)
-			face = np.expand_dims(face, axis=0)
-			# add the face and bounding boxes to their respective
-			# lists
-			faces.append(face)
-			locs.append((startX, startY, endX, endY))
+			face_roi = frame[startY:endY, startX:endX]
+			face_roi = cv2.cvtColor(face_roi, cv2.COLOR_BGR2RGB)
+			face_roi = cv2.resize(face_roi, (224, 224))
+			face_roi = img_to_array(face_roi)
+			face_roi = preprocess_input(face_roi)
+			face_roi = np.expand_dims(face_roi, axis=0)
+			
+			face_roi_list.append(face_roi)
+			face_location_list.append((startX, startY, endX, endY))
 
-	# only make a predictions if at least one face was detected
-	if len(faces) > 0:
-		# for faster inference we'll make batch predictions on *all*
-		# faces at the same time rather than one-by-one predictions
-		# in the above `for` loop
-		preds = maskNet.predict(faces)
+	if len(face_roi_list) > 0:
+		prediction_list = mask_net.predict(face_roi_list)
             
-	# return a 2-tuple of the face locations and their corresponding
-	# locations
-	return (locs, preds)
+	return (face_location_list, prediction_list)
 
 # loop over the frames from the video stream
 while True:
 	# grab the frame from the threaded video stream and resize it
 	# to have a maximum width of 400 pixels
-	frame = vs.read()
+	frame = video_stream.read()
 	frame = imutils.resize(frame, width=400)
 	# detect faces in the frame and determine if they are wearing a
 	# face mask or not
-	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+	(locs, preds) = detect_and_predict_mask(frame, face_net, mask_net)
 
 	# loop over the detected face locations and their corresponding
 	# locations
@@ -129,4 +103,4 @@ while True:
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
-vs.stop()
+video_stream.stop()
